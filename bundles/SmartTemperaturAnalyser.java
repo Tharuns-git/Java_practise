c015c19ee5a4fe4f8dde45d0c8aeef960e588502
Tharuns-git <tharunsanatani@hotmail.com>
2026-03-07 14:40:11 +0530

    After correction --problem 1

--- bundles/SmartTemperaturAnalyser.java
+++ bundles/SmartTemperaturAnalyser.java
@@ -1,43 +1,48 @@
 package bundles;
 import java.util.Scanner;
+
 public class SmartTemperaturAnalyser {
-   public static void main(String[] args) {
-     Scanner sc=new Scanner(System.in);
-     System.out.println("Enter the Temperature in Celcius");
-     double temp=sc.nextDouble();
-     System.out.println("Enter the Humidity percentage ");
-     double Humidity=sc.nextDouble();
-     System.out.println("Enter the windspeed in Km/hr");
-     int windspeed=sc.nextInt();
 
+    public static void main(String[] args) {
 
-     // conversion from Celcius to Fahrenhit
-     // formula = (1C*9/5)+32
-    System.out.println("Temperate :"+temp);
-    double fahrenhit=(temp*(double)9/5)+32;
-    System.out.println("Converted :"+fahrenhit);
-    if(temp<10)
-    {
-         System.out.println("Category:Cold");
-         if(windspeed>40 ||temp <5)
-        System.out.println("Weather Warning");
-            
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Temperature in Celsius:");
+        double temp = sc.nextDouble();
+
+        System.out.println("Enter the Humidity percentage:");
+        double humidity = sc.nextDouble();
+
+        System.out.println("Enter the windspeed in Km/hr:");
+        int windspeed = sc.nextInt();
+
+        // Celsius to Fahrenheit
+        double fahrenheit = (temp * 9 / 5) + 32;
+
+        System.out.println("Temperature: " + temp + "°C");
+        System.out.println("Converted: " + fahrenheit + "°F");
+
+        // Weather Category
+        if (temp < 10) {
+            System.out.println("Category: Cold");
+        } 
+        else if (temp <= 25) {
+            System.out.println("Category: Moderate");
+        } 
+        else {
+            System.out.println("Category: Hot");
+        }
+
+        // Heat Alert
+        if (humidity > 80 && temp > 30) {
+            System.out.println("Alert: Heat Alert");
+        }
+
+        // Weather Warning
+        if (windspeed > 40 || temp < 5) {
+            System.out.println("Alert: Weather Warning");
+        }
+
+        sc.close();
     }
-       
-    else if(temp>=10 && temp<=25)
-        System.out.println("Category:Moderate");
-    else {
-        System.out.println("Category:Hot");
-         if(Humidity>80 && temp>30)
-        System.out.println("Heat Alert");
-
-            
-    }
-        
-
-   
-   
-   }
-
 }
